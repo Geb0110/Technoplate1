28161811146440391dc82dc586ef0281248f6cac
Geb0110 <reyansh.ranjan07@gmail.com>
2021-11-19 10:33:07 +0530

    First commit

---
+++ @main.js
@@ -0,0 +1,11 @@
+function kk()
+{
+    var myStep3 = document.getElementById('STEP_3');
+
+    
+}
+mobiscroll.datepicker('#date-picker', {
+    controls: ['calendar'],
+    selectMultiple: true,
+    selectCounter: true
+});
